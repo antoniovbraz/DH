701a6e17c8f0a42d85022c467f9fae51a88cc141
Antonio Braz <antoniovbraz@gmail.com>
2021-11-16 21:01:53 -0300

    Aula 11 programação imperativa

---
+++ Mochila do Viajante/2.1 PROGRAMACAO IMPERATIVA/modulo-03/aula-11/mesa.js
@@ -0,0 +1,19 @@
+let filmes = ["star wars", "toronto", "rocky", "pulp fiction", "la vida es bella"]
+
+function caixaAlta(array) {
+    for (let i = 0; i < array.length; i++) {
+        console.log(array[i].toUpperCase());
+    }
+}
+
+caixaAlta(filmes);
+
+let filmesOriginais = []
+let filmes = ["toy story", "how to train your dragon", "rocky", "lion king", "frozen"]
+
+function moverDesenhos(array) {
+    for (let i = 0; i < array.length; i++) {
+        const element = array[i];
+        
+    }
+}
