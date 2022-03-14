f55bdbe5e0a63ff08b8913e8ecffb8c6d8331e58
Antonio Braz <antoniovbraz@gmail.com>
2022-03-14 20:24:47 -0300

    organizing my course

---
+++ Mochila do Viajante/Terceiro Bimestre/Front End II/[solução]Aula5_mesa_de_trabalho/Aula5_mesa_de_trabalho[solução]/scripts/script_aula7.js
@@ -0,0 +1,15 @@
+/* 
+Passo a passo para executar o desafio - Aula 7
+1 - Criar a opção de escolha do felino 
+2 - Verificar qual foi a escolha
+3 - Definir uma url da internet
+4 - Alteram o atributo "src"
+*/
+
+let itemsList = document.querySelectorAll('.item');
+
+let idnames = id.forEach(itemsList => {
+    id.id
+});
+
+console.log(idnames)
