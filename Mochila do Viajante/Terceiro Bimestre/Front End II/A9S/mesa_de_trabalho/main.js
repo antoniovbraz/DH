2d1e69f678afc1546004455fa5936f7eba50b38c
Antonio Braz <antoniovbraz@gmail.com>
2022-03-20 19:34:49 -0300

    Revert "Revert "organising my course""
    This reverts commit d3bdfb8f63c3a848f1b574a7f375d76dad1f0235.

---
+++ Mochila do Viajante/Terceiro Bimestre/Front End II/A9S/mesa_de_trabalho/main.js
@@ -0,0 +1,12 @@
+
+const playerOperator = "";
+const playerNumA = 0;
+const playerNumB =0;
+
+function userInteraction(operator, numA, numB) {
+
+ while(playerOperator !== "/" || playerOperator !== "+" || playerOperator !== "-" || playerOperator === ! "*") {
+  prompt("entre com o valor A");
+  }
+
+}
