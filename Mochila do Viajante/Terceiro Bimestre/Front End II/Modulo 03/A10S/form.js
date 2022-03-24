b47046b116bcd7f62d03c9560bafb7d6c36d540f
Antonio Braz <antoniovbraz@gmail.com>
2022-03-23 22:39:31 -0300

    organizing

--- Mochila do Viajante/Terceiro Bimestre/Front End II/Modulo 03/A10S/form.js
+++ Mochila do Viajante/Terceiro Bimestre/Front End II/Modulo 03/A10S/form.js
@@ -2,3 +2,5 @@ let formulario = document.querySelector("form");
 formulario.addEventListener("submit", function (event) {
   event.preventDefault();
 });
+
+const nome_comida = document.getElementsByName("food-name");
