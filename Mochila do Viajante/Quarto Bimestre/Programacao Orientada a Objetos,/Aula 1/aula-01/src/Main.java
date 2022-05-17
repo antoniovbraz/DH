eb8e5f4acc59298fa7dc95ea1bd276221558c6d9
Antonio Braz <antoniovbraz@gmail.com>
2022-05-17 18:13:06 -0300

    Início do Quarto Bimestre - DH

---
+++ Mochila do Viajante/Quarto Bimestre/Programacao Orientada a Objetos,/Aula 1/aula-01/src/Main.java
@@ -0,0 +1,24 @@
+public class Main {
+    public static void main(String[] args) {
+        String nome1 = "Shesheu";
+        int idade = 2;
+        double alimento = 1.5;
+        String resposta = "Au Au";
+
+        System.out.println("Hello, my name is " + nome1 + " I'm " + idade + " years old. \nI eat about " + alimento + " every day.\n"  );
+
+        String nome2 = "Nemo";
+        int idade2 = 1;
+        double alimento2 = 140;
+        String resposta2 = "Glup Glup";
+
+        System.out.println("Hello, my name is " + nome2 + " I'm " + idade2 + " years old. \nI eat about " + alimento2 + " every day.\n"  );
+
+        String nome3 = "Snow";
+        int idade3 = 3;
+        double alimento3 = 500;
+        String resposta3 = "Miau Miau";
+
+        System.out.println("Hello, my name is " + nome3 + " I'm " + idade3 + " years old. \nI eat about " + alimento3 + " every day.\n"  );
+    }
+}
