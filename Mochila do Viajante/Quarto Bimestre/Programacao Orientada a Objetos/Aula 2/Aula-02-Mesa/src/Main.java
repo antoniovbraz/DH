1d6a7163c7d06a3f2d53b6bc8d7ef079a175323e
Antonio Braz <antoniovbraz@gmail.com>
2022-05-18 15:35:30 -0300

    Fixed folder invalid character

---
+++ Mochila do Viajante/Quarto Bimestre/Programacao Orientada a Objetos/Aula 2/Aula-02-Mesa/src/Main.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+// Exercicio 01
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Insira um Número");
+        int num = sc.nextInt();
+        boolean flag = false;
+        for (int i = 2; i <= num / 2; ++i) {
+            // Não é um número primo
+            if (num % i == 0) {
+                flag = true;
+                break;
+            }
+        }
+
+        if (!flag)
+            System.out.println(num + " É Primo");
+        else
+            System.out.println(num + " Não é primo");
+    }
+}
