49f4a201f14360a2b208a16e8053def538a2e4b8
Antonio Braz <antoniovbraz@gmail.com>
2022-11-12 18:58:58 -0300

    Styling my app checkpoint

--- Mochila_do_Viajante/6_Sexto_Bimestre/Front_End_III_V3/Modulo_1/checkpoint1/dragons-app/src/components/Dragon.js
+++ Mochila_do_Viajante/6_Sexto_Bimestre/Front_End_III_V3/Modulo_1/checkpoint1/dragons-app/src/components/Dragon.js
@@ -2,18 +2,22 @@ import { FaTimes } from "react-icons/fa";
 
 const Dragon = ({ dragon, onDelete, onToggle }) => {
   return (
-    <div
-      className={`dragon ${dragon.favorite ? "favorite" : ""}`}
-      onDoubleClick={() => onToggle(dragon.id)}
-    >
-      <h3>
-        {dragon.text}
-        <FaTimes
-          style={{ color: "red", cursor: "pointer" }}
-          onClick={() => onDelete(dragon.id)}
-        />
-      </h3>
-      <p>{dragon.dragonClass}</p>
+    <div className="cards">
+      <div
+        className={`dragon ${dragon.favorite ? "favorite" : ""} card-main`}
+        onDoubleClick={() => onToggle(dragon.id)}
+      >
+        <h3>
+          {dragon.text}
+          <FaTimes
+            style={{ color: "red", cursor: "pointer" }}
+            onClick={() => onDelete(dragon.id)}
+          />
+        </h3>
+        <div className="card-infos">
+          <p>{dragon.dragonClass}</p>
+        </div>
+      </div>
     </div>
   );
 };
